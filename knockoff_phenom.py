a74802b42417dc7306b90c7c7a137b9fcf2b3e0e
Jonathan Taylor <jonathan.taylor@stanford.edu>
2018-03-28 10:26:43 -0700

    sequence of instances dto investigate weird knockoff behavior with sigma unknown

---
+++ knockoff_phenom.py
@@ -0,0 +1,36 @@
+import numpy as np
+from utils import jelena_instance_AR
+
+class small(jelena_instance_AR):
+
+    signature = None
+    name = 'AR(0.5)'
+    n = 500
+    p = 1000
+    s = 30
+    signal = np.sqrt(2 * np.log(p) / n)
+    rho = 0.5
+
+class medium(jelena_instance_AR):
+
+    name = 'AR(0.5)'
+    p = 1000
+    n = 1000
+    signal = np.sqrt(2 * np.log(p) / n)
+
+class large(jelena_instance_AR):
+
+    name = 'AR(0.5)'
+    p = 1000
+    n = 2000
+    signal = np.sqrt(2 * np.log(p) / n)
+
+class larger(jelena_instance_AR):
+
+    name = 'AR(0.5)'
+    p = 1000
+    n = 2500
+    signal = np.sqrt(2 * np.log(p) / n)
+
+small.register(), medium.register(), large.register(), larger.register()
+
