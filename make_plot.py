a71c66037add4747557405c82a9543ffe4b4e1ac
Jonathan Taylor <jonathan.taylor@stanford.edu>
2018-03-29 12:39:21 -0700

    plot changes

--- make_plot.py
+++ make_plot.py
@@ -4,7 +4,8 @@ import pandas as pd
 
 methods = ['ModelX Knockoffs with Sigma (full)',
            'Randomized LASSO + 1SE, smaller noise (selected)',
-           'Randomized LASSO + 1SE, smaller noise (full)']
+           'Randomized LASSO + 1SE, smaller noise (full)',
+           'Lee et al. + theory, aggressive (selected)']
 
 def make_plot_rho_fixed(csvfile, 
                         rho,
@@ -64,11 +65,11 @@ def mixed_signal_plot(rho=0.25):
     fig.clf()
     ax = plt.subplot(121)
     ax, fig = make_plot_rho_fixed('simresults/mixed_n1000_p2000.csv', rho=rho, ylim=(0,1), ax=ax)
-    ax.legend(loc='lower right', fontsize='xx-small')
 
     ax = plt.subplot(122)
     ax, fig = make_plot_rho_fixed('simresults/mixed_n1000_p2000.csv', rho=rho, ylim=(0,1), metric='Full model FDR', ax=ax)
     ax.plot([3.5,5], [0.2, 0.2], 'k--')
+    ax.legend(loc='upper right', fontsize='xx-small')
     fig.savefig('simresults/mixed_rho%0.2f.pdf' % rho)
 
 def AR_signal_plot(rho=0.25):
@@ -77,11 +78,11 @@ def AR_signal_plot(rho=0.25):
     fig.clf()
     ax = plt.subplot(121)
     ax, fig = make_plot_rho_fixed('simresults/AR_n1000_p2000.csv', rho=rho, ylim=(0,1), ax=ax)
-    ax.legend(loc='lower right', fontsize='xx-small')
 
     ax = plt.subplot(122)
     ax, fig = make_plot_rho_fixed('simresults/AR_n1000_p2000.csv', rho=rho, ylim=(0,1), metric='Full model FDR', ax=ax)
     ax.plot([3.5,5], [0.2, 0.2], 'k--')
+    ax.legend(loc='upper right', fontsize='xx-small')
     fig.savefig('simresults/AR_rho%0.2f.pdf' % rho)
 
 def mixed_rho_plot(signal=4):
@@ -90,11 +91,11 @@ def mixed_rho_plot(signal=4):
     fig.clf()
     ax = plt.subplot(121)
     ax, fig = make_plot_signal_fixed('simresults/mixed_n1000_p2000.csv', signal=signal, ylim=(0,1), ax=ax)
-    ax.legend(loc='lower right', fontsize='xx-small')
 
     ax = plt.subplot(122)
     ax, fig = make_plot_signal_fixed('simresults/mixed_n1000_p2000.csv', signal=signal, ylim=(0,1), metric='Full model FDR', ax=ax)
     ax.plot([0, 0.5], [0.2, 0.2], 'k--')
+    ax.legend(loc='upper right', fontsize='xx-small')
     fig.savefig('simresults/mixed_signal%0.1f.pdf' % signal)
 
 def AR_rho_plot(signal=4):
@@ -103,17 +104,17 @@ def AR_rho_plot(signal=4):
     fig.clf()
     ax = plt.subplot(121)
     ax, fig = make_plot_signal_fixed('simresults/AR_n1000_p2000.csv', signal=signal, ylim=(0,1), ax=ax)
-    ax.legend(loc='lower right', fontsize='xx-small')
 
     ax = plt.subplot(122)
     ax, fig = make_plot_signal_fixed('simresults/AR_n1000_p2000.csv', signal=signal, ylim=(0,1), metric='Full model FDR', ax=ax)
     ax.plot([0, 0.5], [0.2, 0.2], 'k--')
+    ax.legend(loc='upper right', fontsize='xx-small')
     fig.savefig('simresults/AR_signal%0.1f.pdf' % signal)
 
 if __name__ == "__main__":
 
-    AR_signal_plot()
-    mixed_signal_plot()
+    AR_signal_plot(rho=0.5)
+    mixed_signal_plot(rho=0.5)
     AR_rho_plot(signal=3.5)
     mixed_rho_plot(signal=3.5)
 
