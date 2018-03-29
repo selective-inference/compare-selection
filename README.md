1885711919a2b96448927150e69efcad116c7bce
Jonathan Taylor <jonathan.taylor@stanford.edu>
2018-03-29 10:25:50 -0700

    making plots based on simresults

--- README.md
+++ README.md
@@ -10,6 +10,7 @@ git submodule update --init
 make install
 cd ../selective-inference
 git submodule update --init
+pip install -r requirements.txt
 pip install -e .
 
 Testing
@@ -18,3 +19,14 @@ Testing
 If installed correctly, the following should give you some results for a small problem:
 
 python compare.py --instance indep_instance --nsample 100 --nfeature 50 --nsignal 10 --methods lee_theory liu_theory --htmlfile indep.html
+
+For a grid of signal and / or AR(1) parameters try
+
+python compare.py --instance indep_instance --nsample 100 --nfeature 50 --nsignal 10 --methods lee_theory liu_theory --htmlfile indep.html --signal 3 4 5 --rho 0 0.5
+
+Also, try 
+
+python compare.py --help
+python compare.py --list_instances
+python compare.py --list_methods
+
---
+++ make_plot.py
@@ -0,0 +1,119 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+methods = ['ModelX Knockoffs with Sigma (full)',
+           'Randomized LASSO + 1SE, smaller noise (selected)',
+           'Randomized LASSO + 1SE, smaller noise (full)']
+
+def make_plot_rho_fixed(csvfile, 
+                        rho,
+                        metric='Full model power',
+                        methods=methods,
+                        ylim=None,
+                        ax=None):
+    df = pd.read_csv(csvfile)
+
+    df = df[df['rho'] == rho]
+    signal = sorted(np.unique(df['signal']))
+
+    fig = plt.gcf()
+    if ax is None:
+        ax = fig.gca()
+    for method in methods:
+        df_ = df[df[df.columns[1]] == method]
+        ax.plot(signal, df_[metric], label=method)
+        print(method)
+    if ylim:
+        ax.set_ylim(ylim)
+    ax.set_xlim([min(signal), max(signal)])
+    ax.set_xlabel('Signal size')
+    ax.set_ylabel(metric)
+
+    return ax, fig
+
+def make_plot_signal_fixed(csvfile, 
+                           signal,
+                           metric='Full model power',
+                           methods=methods,
+                           ylim=None,
+                           ax=None):
+    df = pd.read_csv(csvfile)
+
+    df = df[df['signal'] == signal]
+    rho = sorted(np.unique(df['rho']))
+
+    fig = plt.gcf()
+    if ax is None:
+        ax = fig.gca()
+    for method in methods:
+        df_ = df[df[df.columns[1]] == method]
+        ax.plot(rho, df_[metric], label=method)
+        print(method)
+    if ylim:
+        ax.set_ylim(ylim)
+    ax.set_xlim([min(rho), max(rho)])
+    ax.set_xlabel(r'AR parameter $\rho$')
+    ax.set_ylabel(metric)
+
+    return ax, fig
+
+def mixed_signal_plot(rho=0.25):
+
+    fig = plt.figure(num=1, figsize=(10, 5))
+    fig.clf()
+    ax = plt.subplot(121)
+    ax, fig = make_plot_rho_fixed('simresults/mixed_n1000_p2000.csv', rho=rho, ylim=(0,1), ax=ax)
+    ax.legend(loc='lower right', fontsize='xx-small')
+
+    ax = plt.subplot(122)
+    ax, fig = make_plot_rho_fixed('simresults/mixed_n1000_p2000.csv', rho=rho, ylim=(0,1), metric='Full model FDR', ax=ax)
+    ax.plot([3.5,5], [0.2, 0.2], 'k--')
+    fig.savefig('simresults/mixed_rho%0.2f.pdf' % rho)
+
+def AR_signal_plot(rho=0.25):
+
+    fig = plt.figure(num=1, figsize=(10, 5))
+    fig.clf()
+    ax = plt.subplot(121)
+    ax, fig = make_plot_rho_fixed('simresults/AR_n1000_p2000.csv', rho=rho, ylim=(0,1), ax=ax)
+    ax.legend(loc='lower right', fontsize='xx-small')
+
+    ax = plt.subplot(122)
+    ax, fig = make_plot_rho_fixed('simresults/AR_n1000_p2000.csv', rho=rho, ylim=(0,1), metric='Full model FDR', ax=ax)
+    ax.plot([3.5,5], [0.2, 0.2], 'k--')
+    fig.savefig('simresults/AR_rho%0.2f.pdf' % rho)
+
+def mixed_rho_plot(signal=4):
+
+    fig = plt.figure(num=1, figsize=(10, 5))
+    fig.clf()
+    ax = plt.subplot(121)
+    ax, fig = make_plot_signal_fixed('simresults/mixed_n1000_p2000.csv', signal=signal, ylim=(0,1), ax=ax)
+    ax.legend(loc='lower right', fontsize='xx-small')
+
+    ax = plt.subplot(122)
+    ax, fig = make_plot_signal_fixed('simresults/mixed_n1000_p2000.csv', signal=signal, ylim=(0,1), metric='Full model FDR', ax=ax)
+    ax.plot([0, 0.5], [0.2, 0.2], 'k--')
+    fig.savefig('simresults/mixed_signal%0.1f.pdf' % signal)
+
+def AR_rho_plot(signal=4):
+
+    fig = plt.figure(num=1, figsize=(10, 5))
+    fig.clf()
+    ax = plt.subplot(121)
+    ax, fig = make_plot_signal_fixed('simresults/AR_n1000_p2000.csv', signal=signal, ylim=(0,1), ax=ax)
+    ax.legend(loc='lower right', fontsize='xx-small')
+
+    ax = plt.subplot(122)
+    ax, fig = make_plot_signal_fixed('simresults/AR_n1000_p2000.csv', signal=signal, ylim=(0,1), metric='Full model FDR', ax=ax)
+    ax.plot([0, 0.5], [0.2, 0.2], 'k--')
+    fig.savefig('simresults/AR_signal%0.1f.pdf' % signal)
+
+if __name__ == "__main__":
+
+    AR_signal_plot()
+    mixed_signal_plot()
+    AR_rho_plot(signal=3.5)
+    mixed_rho_plot(signal=3.5)
+
